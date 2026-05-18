4bb50d8e63f595f32cdb854a9d8a5c13d6d73bbb
Frank Pereira <frankpereirr.working@gmail.com>
2026-05-18 16:44:30 -0400

    feat: add configuracoes hibernate

---
+++ src/main/java/app/util/HibernateUtil.java
@@ -0,0 +1,18 @@
+package app.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class HibernateUtil {
+
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory(
+                    "study-persistence-unit"
+            );
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+}
