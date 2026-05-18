5a34a83ca7fd12829c40d2e3b5cbc56169d55d97
Frank Pereira <frankpereirr.working@gmail.com>
2026-05-18 16:39:10 -0400

    feat: add entity Text

---
+++ src/main/java/app/domain/text/Text.java
@@ -0,0 +1,58 @@
+package app.domain.text;
+
+import app.domain.study.Study;
+import app.domain.topic.Topic;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "text")
+public class Text {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "text_id")
+    private Long id;
+
+    @Column(name = "text_content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", foreignKey = @ForeignKey(name = "fk_text_study"))
+    private Study study;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id", foreignKey = @ForeignKey(name = "fk_text_topic"))
+    private Topic topic;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Study getStudy() {
+        return study;
+    }
+
+    public void setStudy(Study study) {
+        this.study = study;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+}
