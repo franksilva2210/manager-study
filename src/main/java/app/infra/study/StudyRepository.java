7f1ddd36ecf1bdbd67f9821de76d9dee015644b0
Frank Pereira <franksilva.2210@gmail.com>
2026-05-18 16:41:29 -0400

    feat: add camadas de repository de Text, Study e Topic

---
+++ src/main/java/app/infra/study/StudyRepository.java
@@ -0,0 +1,129 @@
+package app.infra.study;
+
+import app.domain.study.Study;
+import app.util.HibernateUtil;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class StudyRepository {
+
+    public void save(Study study) {
+
+        EntityManager em =
+                HibernateUtil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+
+            em.persist(study);
+
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw e;
+
+        } finally {
+
+            em.close();
+        }
+    }
+
+    public Study update(Study study) {
+
+        EntityManager em =
+                HibernateUtil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+
+            Study merged = em.merge(study);
+
+            em.getTransaction().commit();
+
+            return merged;
+
+        } catch (Exception e) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw e;
+
+        } finally {
+
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+
+        EntityManager em =
+                HibernateUtil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+
+            Study study =
+                    em.find(Study.class, id);
+
+            if (study != null) {
+                em.remove(study);
+            }
+
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw e;
+
+        } finally {
+
+            em.close();
+        }
+    }
+
+    public Study findById(Long id) {
+
+        EntityManager em =
+                HibernateUtil.getEntityManager();
+
+        try {
+
+            return em.find(Study.class, id);
+
+        } finally {
+
+            em.close();
+        }
+    }
+
+    public List<Study> findAll() {
+        EntityManager em = HibernateUtil.getEntityManager();
+        try {
+            return em.createQuery(
+                    """
+                    SELECT DISTINCT s
+                    FROM Study s
+                    """,
+                    Study.class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+}
