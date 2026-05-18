31a70460479c6b753db7f981958801d8da93064c
Frank Pereira <frankpereirr.working@gmail.com>
2026-05-18 16:49:17 -0400

    feat: add processamento para obtenção de estudos

--- src/main/java/app/ui/main/MainService.java
+++ src/main/java/app/ui/main/MainService.java
@@ -1,21 +1,53 @@
 package app.ui.main;
 
 import app.domain.study.Study;
-import app.infra.study.StudyDao;
+import app.domain.text.Text;
+import app.domain.topic.Topic;
+import app.infra.study.StudyRepository;
+import app.infra.text.TextRepository;
+import app.infra.topic.TopicRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainService {
 
-    private final StudyDao studyDao = new StudyDao();
+    StudyRepository studyRepository = new StudyRepository();
+    TopicRepository topicRepository = new TopicRepository();
+    TextRepository textRepository = new TextRepository();
 
     public List<Study> consultStudyAll() {
         try {
-            return studyDao.consultAll();
+            List<Study> listStudy = studyRepository.findAll();
+            List<Topic> listTopic = topicRepository.findAllRootTopics();
+            List<Text> listText = textRepository.findAllStudyTexts();
+
+            Map<Long, List<Topic>> mapTopicsByStudy = listTopic.stream()
+                    .collect(Collectors.groupingBy(
+                            t -> t.getStudy().getId()
+                    ));
+
+            Map<Long, List<Text>> mapTextsByStudy = listText.stream()
+                    .collect(Collectors.groupingBy(
+                            t -> t.getStudy().getId()
+                    ));
+
+            for (Study study : listStudy) {
+                study.setListTopics(
+                        mapTopicsByStudy.getOrDefault(study.getId(), new ArrayList<>())
+                );
+
+                study.setListText(
+                        mapTextsByStudy.getOrDefault(study.getId(), new ArrayList<>())
+                );
+            }
+
+            return listStudy;
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao buscar estudos. ", e);
         }
     }
-
 
 }
