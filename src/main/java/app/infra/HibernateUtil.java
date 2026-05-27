479f3f62d4cb1307cf6cd357d8deb8a4ca299970
Frank Pereira <franksilva.2210@gmail.com>
2026-05-27 10:33:02 -0400

    chore: migração de banco de dados PostgreSQL para SQLite

--- src/main/java/app/infra/HibernateUtil.java
+++ src/main/java/app/infra/HibernateUtil.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HibernateUtil {
 
     private static EntityManagerFactory emf;
@@ -11,8 +14,16 @@ public class HibernateUtil {
     public static void initialize() {
 
         if (emf == null) {
+            Map<String, Object> properties = new HashMap<>();
+
+            properties.put(
+                    "jakarta.persistence.jdbc.url",
+                    SQLiteDataBaseConfig.JDBC_URL
+            );
+
             emf = Persistence.createEntityManagerFactory(
-                    "knowledge-manager-persistence-unit"
+                    "study-manager-persistence-unit",
+                    properties
             );
 
             System.out.println("Hibernate initialized.");
