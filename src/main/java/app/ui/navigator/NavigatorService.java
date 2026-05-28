7a572b400da599c27dca4d9fa998562b2d9707ec
Frank Pereira <frankpereirr.working@gmail.com>
2026-05-28 09:29:28 -0400

    chore: deixa navigator como recurso futuro

--- src/main/java/app/ui/navigator/NavigatorService.java
+++ src/main/java/app/ui/navigator/NavigatorService.java
@@ -1,7 +1,5 @@
 package app.ui.navigator;
 
-import app.AppBootstrap;
-import app.infra.HibernateUtil;
 import javafx.stage.Stage;
 
 public class NavigatorService {
@@ -14,23 +12,6 @@ public class NavigatorService {
 
     public static void restart() {
 
-        try {
-
-            HibernateUtil.shutdown();
-
-            primaryStage.close();
-
-            Stage newStage = new Stage();
-
-            new AppBootstrap().start(newStage);
-
-        } catch (Exception e) {
-
-            throw new RuntimeException(
-                    "Erro ao reiniciar aplicação.",
-                    e
-            );
-        }
     }
 
 }
