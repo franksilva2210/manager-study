9f7e21bf47a373fd591a6a39bb4c78b66f642cd7
Frank Pereira <franksilva.2210@gmail.com>
2026-07-12 20:59:46 -0400

    refator

--- src/main/java/app/ui/pane/right/DragDroppedService.java
+++ src/main/java/app/ui/pane/right/DragDroppedService.java
@@ -1,11 +1,12 @@
 package app.ui.pane.right;
 
+import app.application.study.StudyDTO;
 import app.application.topic.TopicDTO;
 import app.application.topic.TopicMapper;
 import app.domain.topic.Topic;
 import app.infra.topic.TopicRepository;
 
-public class ConfigDragDroppedListViewService {
+public class DragDroppedService {
 
     private TopicRepository topicRepository = new TopicRepository();
 
@@ -31,6 +32,16 @@ public class ConfigDragDroppedListViewService {
         topicDragged.setTopicParentId(topicDestination.getId());
 
         topicRepository.moveTopic(topicDragged);
+    }
+
+    public void moveTopicToStudy(
+            TopicDTO topicDragged,
+            StudyDTO studyDestination) {
+
+        if (topicDragged == null || studyDestination == null) {
+            return;
+        }
+
     }
 
 }
