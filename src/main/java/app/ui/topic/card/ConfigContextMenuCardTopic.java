c04a63cb6e3244ddcbb747a9a7d534e7acecf438
Frank Pereira <franksilva.2210@gmail.com>
2026-07-16 19:25:11 -0400

    feat: Esconde o menu ao clicar em outro lugar

--- src/main/java/app/ui/topic/card/ConfigContextMenuCardTopic.java
+++ src/main/java/app/ui/topic/card/ConfigContextMenuCardTopic.java
@@ -65,6 +65,8 @@ public class ConfigContextMenuCardTopic {
                     event.getScreenY()
             );
         });
+
+        root.setOnMousePressed(e -> contextMenu.hide());
     }
 
 }
