15da9f9a522de6b167dbae49b447e316c700550e
Frank Pereira <franksilva.2210@gmail.com>
2026-07-22 23:19:42 -0400

    feat: adiciona verificação de documento em edição ao acessar tela de backup

--- src/main/java/app/ui/main/ScreenMainController.java
+++ src/main/java/app/ui/main/ScreenMainController.java
@@ -108,6 +108,10 @@ public class ScreenMainController implements Initializable {
 	}
 
 	private void openScreenBackup() {
+		if (!confirmChangeStudyOrTopic()) {
+			return;
+		}
+
 		ScreenBackupController controller = new ScreenBackupController();
 		ScreenBackupWindow window = new ScreenBackupWindow(stage, controller);
 		window.showScreen();
