fa5d4c3aaf9b0b7a74f06b50a5055ea120dfdbc9
Frank Pereira <franksilva.2210@gmail.com>
2026-07-24 13:14:47 -0400

    feat: adiciona renderização de tabelas, texto cortado, link e emoji para editor markdown

--- src/main/java/app/ui/document/edit/MarkdownConverter.java
+++ src/main/java/app/ui/document/edit/MarkdownConverter.java
@@ -1,16 +1,33 @@
 package app.ui.document.edit;
 
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.emoji.EmojiExtension;
+import com.vladsch.flexmark.ext.emoji.EmojiImageType;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+
+import java.util.List;
 
 public class MarkdownConverter {
 
+    private static final MutableDataSet OPTIONS = new MutableDataSet()
+            .set(Parser.EXTENSIONS, List.of(
+                    TablesExtension.create(),
+                    StrikethroughExtension.create(),
+                    AutolinkExtension.create(),
+                    EmojiExtension.create()
+            ))
+            .set(EmojiExtension.USE_IMAGE_TYPE, EmojiImageType.UNICODE_ONLY);
+
     private static final Parser PARSER =
-            Parser.builder().build();
+            Parser.builder(OPTIONS).build();
 
     private static final HtmlRenderer RENDERER =
-            HtmlRenderer.builder().build();
+            HtmlRenderer.builder(OPTIONS).build();
 
     public static String toHtml(String markdown) {
         Node document = PARSER.parse(markdown);
